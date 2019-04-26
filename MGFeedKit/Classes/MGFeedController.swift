5b54405d96a90f5821539be17cb120b24b77e0a4
haraldbregu <harald.bregu@gmail.com>
2019-04-26 18:12:58 +0200

    update some

--- MGFeedKit/Classes/MGFeedController.swift
+++ MGFeedKit/Classes/MGFeedController.swift
@@ -86,11 +86,17 @@ public class MGFeedController: UIViewController {
         refreshControl.addTarget(self, action: #selector(reloadData(button:)), for: .valueChanged)
         tableView.refreshControl = refreshControl
 
-        activityIndicatorView = UIActivityIndicatorView(style: .white)
-        activityIndicatorView.center = tableView.center
+        activityIndicatorView = UIActivityIndicatorView()
+        activityIndicatorView.style = .white
         view.addSubview(activityIndicatorView)
 
         setFeed(assets.data)
+    }
+    
+    public override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        activityIndicatorView.center = CGPoint(x: (UIScreen.main.bounds.width / 2.0), y: (UIScreen.main.bounds.height / 2.0))
     }
     
     @objc private func navigationItemAction(barButtonItem: UIBarButtonItem) {
@@ -106,7 +112,7 @@ public class MGFeedController: UIViewController {
         guard let url = URL(string: data.url) else { return }
         activityIndicatorView.startAnimating()
         let parser = FeedParser(URL: url)
-        parser.parseAsync(queue: .global(qos: .userInitiated)) { [unowned self] (result) in
+        parser.parseAsync(queue: .global(qos: .userInitiated)) { (result) in
             switch result {
             case .atom(let feed):
                 self.atomFeed(feed)
@@ -114,8 +120,10 @@ public class MGFeedController: UIViewController {
             case .rss(let feed):
                 self.rssFeed(feed)
                 break
-            case .json: break
-            case .failure: break
+            case .json:
+                break
+            case .failure:
+                break
             }
             
             DispatchQueue.main.async {
@@ -135,7 +143,6 @@ public class MGFeedController: UIViewController {
                     let doc: Document = try SwiftSoup.parse(html)
                     let link: Element? = try doc.select("img").first()
                     let linkHref: String = try link?.attr("src") ?? ""
-
                     let item = MGFeedItem()
                     item.title = feedItem.title ?? ""
                     item.imageUrl = linkHref
